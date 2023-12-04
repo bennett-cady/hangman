345c5af46a2c35c3e8f8fb91e33eebf4a06d328a
bennett-cady <bennett.cady14@gmail.com>
2023-12-04 13:55:55 -0700

    first commit

---
+++ hangman-console-game/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace hangman_console_game
+{
+
+    internal partial class Program
+    {
+
+        /*
+         * TO-DO
+         * 
+         * Read from a file of potential words/phrases
+         * Randomly select word/phrase from this list
+         * Display number of guesses used/ total amount
+         * Display the hidden word, filled in with correct guesses
+         * Display a list of incorrect guesses
+         */
+
+        public static void PlayGame()
+        {
+            byte guesses = 0;
+            bool found = false;
+
+            Console.WriteLine("Welcome to hangman! Press y if you would like to play");
+            String a = Console.ReadLine();
+            if (a.Length > 0 && a.Equals("y"))
+            {
+                Console.WriteLine("Great! Let's get started");
+            }
+            else
+            {
+                Console.WriteLine("Okay, maybe later...");
+            }
+            /*
+            while(guesses<7 || found==false)
+            {
+                Console.WriteLine('Play game here');
+
+                if(!word.contains('-')) 
+                {
+                    found = true;
+                }
+            }
+            */
+
+
+            string word = "Choose word";
+            string output = StringOperations.HideWord(word);
+            Console.WriteLine(output);
+            Console.WriteLine(StringOperations.Replace(output, word, 'o'));   
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            PlayGame();
+        }
+    }
+}
---
+++ hangman-console-game/StringOperations.cs
@@ -0,0 +1,52 @@
+﻿namespace hangman_console_game
+{
+
+    internal partial class Program
+    {
+        public class StringOperations
+        {
+
+            public static string HideWord(string text)
+            {
+                string hidden = "";
+                foreach (char c in text)
+                {
+                    if (c == ' ')
+                    {
+                        hidden += " ";
+                    }
+                    else
+                    {
+                        hidden += "-";
+                    }
+                }
+                return hidden;
+            }
+
+
+            public static string Replace(string hidden, string word, char guess)
+            {
+                char[] hiddenAsArray = hidden.ToCharArray();
+                for(int i = 0; i<word.Length; i++)
+                {
+                    if (word[i] == guess)
+                    {
+                        hiddenAsArray[i] = guess;   
+                    }
+                }
+                hidden = "";
+                foreach (char c in hiddenAsArray)
+                {
+                    hidden += c;
+                }
+                return hidden;
+            }
+
+            public static bool Guess(string text, char guess)
+            {
+                return text.Contains(guess);
+            }
+
+        }
+    }
+}
